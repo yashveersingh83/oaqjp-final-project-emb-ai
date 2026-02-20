78f082a573dc653eb175a374a2b50408a27d2448
Yashveer <yashveersingh83@gmail.com>
2026-02-20 21:45:19 +0100

    Implement emotion detection function

---
+++ 3a_output_formatting.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+
+def emotion_detector(text_to_analyse):
+    if text_to_analyse is None:
+        return {'message':'Please enter some text as input'}
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    inputjson = { "raw_document": { "text": text_to_analyse } }
+    try:
+      response = requests.post(url,json=inputjson,headers=headers)
+      formatted_output = json.loads(response.text)      
+      #print(formatted_output)
+      label = formatted_output['emotionPredictions'][0]['emotion']
+      print (label)
+    except:
+        print('Error occured')
