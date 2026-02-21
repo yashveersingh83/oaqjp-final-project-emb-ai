3d1470cfe4ed6278ea305139188b44adc332b3ce
yashveersingh83 <yashveersingh83@gmail.com>
2026-02-21 12:44:59 -0500

    error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,7 +1,7 @@
 import requests
 def emotion_detector(text_to_analyse):
-    if not text_to_analyse:
-        return None
+    #if not text_to_analyse:
+    #    return None
 
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
@@ -10,10 +10,26 @@ def emotion_detector(text_to_analyse):
     try:
         response = requests.post(url, json=inputjson, headers=headers)
         response.raise_for_status()
+        print (f' res: {response}')
+        if response.status_code == 400:
+            return {
+                'anger': None,
+                'disgust': None,
+                'fear': None,
+                'joy': None,
+                'sadness': None
+            },400
+
         formatted_output = response.json()
 
-        return formatted_output['emotionPredictions'][0]['emotion']
+        return formatted_output['emotionPredictions'][0]['emotion'],200
 
-    except requests.exceptions.RequestException as e:
+    except Exception as e:
         print("API Error:", e)
-        return None
+        return {
+                'anger': None,
+                'disgust': None,
+                'fear': None,
+                'joy': None,
+                'sadness': None
+            }
